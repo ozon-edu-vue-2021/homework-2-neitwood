c8011f9697b333c53ad63510b0d0efe7e5963b08
neit <neitwood@gmail.com>
2021-10-26 15:35:34 +0300

    added vue-svg-loader, added specialization for NodeElement and sort nodes by type

---
+++ src/components/NodeElement.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="node-element">
+    <FolderSvg
+        v-if="node.type==='directory'"
+        class="icon"
+    />
+    <div class="caption">
+      {{ node.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import FolderSvg from '../../public/static/folder.svg';
+
+export default {
+  name: "NodeElement",
+  props: {
+    node: Object
+  },
+  components: {
+    FolderSvg
+  }
+}
+</script>
+
+<style scoped>
+  .node-element{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 2px;
+  }
+  .caption{
+    text-align: left;
+    color: #eaeaea;
+    font-family: sans-serif;
+    font-size: 14px;
+    font-weight: lighter;
+    padding-left: 5px;
+  }
+  .icon{
+    fill: #86929a;
+  }
+</style>
--- src/components/TreeFilesNode.vue
+++ src/components/TreeFilesNode.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="node" >
+  <div class="node">
 
-    <div class="name" @click="openClose">
-      <span class="type">
-        {{node.type==='file'?'File':''}}
-        {{node.type==='directory'?'Dir':''}}
-      </span>
-      {{ node.name }}
+    <div class="name" @click="openClose(node)">
+      <NodeElement :node="node"/>
     </div>
 
     <div v-if="opened">
       <tree-files-node
-          v-for="dir in node.contents"
+          v-for="dir in currentNode"
           :key="dir.name"
           :node="dir"
       />
@@ -21,39 +17,57 @@
 </template>
 
 <script>
+import NodeElement from "./NodeElement";
+
 export default {
   name: "TreeFilesNode",
   props: {
     node: Object
   },
-  data(){
-    return{
-      opened: false
+  data() {
+    return {
+      opened: false,
+      currentNode: null
     }
   },
-  methods:{
-    openClose(){
+  methods: {
+    openClose(node) {
+      this.currentNode = node;
       this.opened = !this.opened;
     },
+  },
+  watch: {
+    currentNode: function (val) {
+      this.currentNode = val.contents.sort((a,b)=> {
+            if (a.type < b.type)
+              return -1;
+            return 0;
+          }
+      );
+    },
+  },
+  computed: {
 
-  }
+  },
+  components: {
+    NodeElement,
+  },
 }
 </script>
 
 <style scoped>
-  .node{
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 100%;
-    padding-left: 10px;
-  }
-  .type{
-    color: blue;
-  }
-  .name{
-    width: 100%;
-    text-align: left;
-    color: #bababb;
-  }
+.node{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  padding-left: 15px;
+  cursor: pointer;
+}
+.type{
+  color: blue;
+}
+.name{
+  width: 100%;
+}
 </style>
