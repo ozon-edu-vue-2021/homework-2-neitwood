a2c0162e616c211be3b58d6409ecb1073e146d51
neit <neitwood@gmail.com>
2021-10-26 12:42:58 +0300

    added TreeFiles wrapper and TreeFilesNode recursive component, also implemented indentation, expansion

---
+++ src/components/TreeFiles.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="folders">
+      <TreeFilesNode :node="json"></TreeFilesNode>
+  </div>
+</template>
+
+<script>
+import TreeFilesNode from "./TreeFilesNode";
+
+export default {
+  name: 'TreeFiles',
+  props: {
+    json: Object
+  },
+  components: {
+    TreeFilesNode
+  }
+}
+</script>
+
+<style scoped>
+  .folders{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 400px;
+    height: 100vh;
+    overflow-y: scroll;
+    padding-left: 10px;
+    background-color: #3c3f41;
+  }
+</style>
---
+++ src/components/TreeFilesNode.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="node" >
+
+    <div class="name" @click="openClose">
+      <span class="type">
+        {{node.type==='file'?'File':''}}
+        {{node.type==='directory'?'Dir':''}}
+      </span>
+      {{ node.name }}
+    </div>
+
+    <div v-if="opened">
+      <tree-files-node
+          v-for="dir in node.contents"
+          :key="dir.name"
+          :node="dir"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TreeFilesNode",
+  props: {
+    node: Object
+  },
+  data(){
+    return{
+      opened: false
+    }
+  },
+  methods:{
+    openClose(){
+      this.opened = !this.opened;
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+  .node{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    padding-left: 10px;
+  }
+  .type{
+    color: blue;
+  }
+  .name{
+    width: 100%;
+    text-align: left;
+    color: #bababb;
+  }
+</style>
